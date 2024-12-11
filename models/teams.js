822164f1ea294ea130cbe444275663df6e6209ac
Cesar Preza <cpreza93@yahoo.com>
2024-12-10 20:52:06 -0800

    feat: added model

--- models/teams.js
+++ models/teams.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const teamSchema = new mongoose.Schema({
     name: String,
     division: String,
-    founded: number,
+    founded: Number,
 });
 
 const Team = mongoose.model('Team', teamSchema);
