2d014dd24409fe84784b790607124d7873fc308f
Cesar Preza <cpreza93@yahoo.com>
2024-12-15 13:20:58 -0800

    feat: adds post route

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const methodOverride = require('method-override');
 const app = express();
 const Team = require('./models/teams');
 const teams = require('./data/teams');
+const mongoose = require('mongoose');
 
 require('./configs/database');
 
 // ********************
 //    MIDDLEWARE
 // ********************
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 
@@ -21,7 +22,6 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 //app.use(express.static(path.join(__dirname, 'public')));
-
 // ROUTES
 app.get('/', (req, res) => {
     res.render('./home/index');
@@ -37,19 +37,21 @@ app.get('/teams/new', (req, res) => {
 });
 
 app.post('/teams', async (req, res) => {
-    // const {
-    //     name = 'new team',
-    //     division = 'division',
-    //     founded = 'year founded',
-    // } = req.body;
+    const {
+        name = 'new team',
+        division = 'division',
+        founded = 'founded',
+    } = req.body;
 
     const newTeam = new Team({
-        name: req.body.name,
-        division: req.body.division,
-        founded: req.body.founded,
+        name: name,
+        division: division,
+        founded: founded,
     });
-    await Team.create(req.body);
+    await newTeam.save();
+    teams.push(newTeam);
     console.log(teams);
+    console.log(req.body);
     res.status(201).redirect('/teams');
 });
 
